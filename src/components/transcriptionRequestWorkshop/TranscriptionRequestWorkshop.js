e13030d9c9a1c77313a7724b018a0248e5517876
Jacob Eckert <jweckert17@gmail.com>
2020-09-14 14:12:30 -0500

    adding TranscriptionRequestControl and rendering it in workshop

--- src/components/transcriptionRequestWorkshop/TranscriptionRequestWorkshop.js
+++ src/components/transcriptionRequestWorkshop/TranscriptionRequestWorkshop.js
@@ -2,14 +2,18 @@ import React, { useState } from 'react';
 import YouTube from 'react-youtube';
 
 import YouTubeSearchBar from '../youTubeSearchBar/YouTubeSearchBar';
+import TranscriptionRequestControl from './TranscriptionRequestControl/TranscriptionRequestControl';
 
 const TranscriptionRequestWorkshop = () => {
   const [ videoId, setVideoId ] = useState('');
+  const [ startTime, setStartTime ] = useState(null);
 
   return (
     <section className="workshop">
       <YouTubeSearchBar value={videoId} onChange={setVideoId} />
       <YouTube videoId={videoId} />
+
+      <TranscriptionRequestControl isRequesting={startTime} />
     </section>
   );
 };
