4e7dde500417bca77fdbd5f5299bbd794ecd9b0f
Jacob Eckert <jweckert17@gmail.com>
2020-09-14 14:26:40 -0500

    getting reference to youtube player when video cued in workshop, handling transcription request control click in workshop

--- src/components/transcriptionRequestWorkshop/TranscriptionRequestWorkshop.js
+++ src/components/transcriptionRequestWorkshop/TranscriptionRequestWorkshop.js
@@ -1,19 +1,45 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import YouTube from 'react-youtube';
 
 import YouTubeSearchBar from '../youTubeSearchBar/YouTubeSearchBar';
 import TranscriptionRequestControl from './TranscriptionRequestControl/TranscriptionRequestControl';
 
 const TranscriptionRequestWorkshop = () => {
+  const [ player, setPlayer ] = useState(null);
   const [ videoId, setVideoId ] = useState('');
   const [ startTime, setStartTime ] = useState(null);
+
+  const handleYouTubeStateChange = e => {
+    if(e.data === YouTube.PlayerState.CUED) {
+      setPlayer(e.target);
+    }
+  };
+
+  const handleTranscriptionRequestControlClick = () => {
+    const timeClicked = player.getCurrentTime();
+
+    if(startTime === null) {
+      setStartTime(timeClicked);
+    }
+    else {
+      const transcriptionRequestData = {
+        videoId,
+        startTime,
+        endTime: timeClicked
+      };
+
+      console.log(transcriptionRequestData);
+
+      setStartTime(null);
+    }
+  };
 
   return (
     <section className="workshop">
       <YouTubeSearchBar value={videoId} onChange={setVideoId} />
-      <YouTube videoId={videoId} />
+      <YouTube videoId={videoId} onStateChange={handleYouTubeStateChange} />
 
-      <TranscriptionRequestControl isRequesting={startTime} />
+      <TranscriptionRequestControl isRequesting={!!startTime} onClick={handleTranscriptionRequestControlClick} />
     </section>
   );
 };
