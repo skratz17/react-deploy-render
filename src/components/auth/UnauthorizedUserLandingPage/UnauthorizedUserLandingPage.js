461c2885e4166bbb0950776efe23534d8a54008e
Jacob Eckert <jweckert17@gmail.com>
2020-09-14 09:32:36 -0500

    deleted some boilerplate, added basic structure for logged in check rendering app vs login/registration page

---
+++ src/components/auth/UnauthorizedUserLandingPage/UnauthorizedUserLandingPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const UnauthorizedUserLandingPage = props => (
+  <div>請登入或註冊</div>
+);
+
+export default UnauthorizedUserLandingPage;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+import UhhhWut from './components/UhhhWut';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
+    <BrowserRouter>
+      <UhhhWut />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
