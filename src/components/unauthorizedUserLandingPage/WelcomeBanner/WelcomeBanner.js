31425fb80de63b292019b76601ce7b56d6393987
Jacob Eckert <jweckert17@gmail.com>
2020-09-16 14:43:57 -0500

    refactoring animation logic to only use requestAnimationFrame, no more setTimeout/setInterval

--- src/components/unauthorizedUserLandingPage/WelcomeBanner/WelcomeBanner.js
+++ src/components/unauthorizedUserLandingPage/WelcomeBanner/WelcomeBanner.js
@@ -9,59 +9,64 @@ const WELCOME_MESSAGES = [
   '歡迎來到UHHHWUT'
 ];
 
+/*
+How long you want your whole animation cycle to take, in ms.
+One animation cycle consists of three steps:
+  1) Welcome header being fully opaque
+  2) Welcome header fading out to be fully transparent
+  3) Welcome header fading back in to be fully visible
+*/
+const ANIMATION_DURATION = 5000;
+
+// How long into the ANIMATION_DURATION that you want to begin the fading out animation, in ms.
+const FADE_OUT_POINT = 3000;
+
+// How long into the ANIMATION_DURATION that you want to begin the fading back in animation, in ms.
+const FADE_IN_POINT = 4000;
+
 const WelcomeBanner = () => {
   const [ welcomeMessageIndex, setWelcomeMessageIndex ] = useState(0);
-  const [ isFadeOut, setIsFadeOut ] = useState(false);
 
   const welcomeHeader = useRef(null);
 
   useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      setIsFadeOut(true);
-    }, 4000);
-
-    return () => { clearTimeout(timeoutId); };
-  }, [ welcomeMessageIndex ]);
-
-  useEffect(() => {
-    let timeoutId;
+    let start;
+    let prev;
     let animationId;
+    let hasResetWelcomeMessage = false;
 
-    if(isFadeOut) {
-      let start;
-      let prev;
+    const animationCallback = timestamp => {
+      if(start === undefined) start = timestamp;
+      if(prev === undefined) prev = 0;
+      const elapsed = timestamp - start;
 
-      const animationCallback = timestamp => {
-        if(start === undefined) start = timestamp;
-        const elapsed = timestamp - start;
-
-        if(prev === undefined || Math.floor(elapsed / 50) > prev) {
-          prev = Math.floor(elapsed / 100);
-          let factor;
-          if(elapsed <=1000) {
-            factor = 1 - (elapsed / 1000);
-          }
-          else {
-            factor = elapsed / 2000;
-          }
-          welcomeHeader.current.style.opacity = factor;
+      if(elapsed >= FADE_OUT_POINT && elapsed <= ANIMATION_DURATION) {
+        let opacity;
+        prev = elapsed;
+        if(elapsed <= FADE_IN_POINT) {
+          opacity = 1 - ((elapsed - FADE_OUT_POINT) / (FADE_IN_POINT - FADE_OUT_POINT));
         }
-
-        if(elapsed < 2000) {
-          requestAnimationFrame(animationCallback);
+        else {
+          if(!hasResetWelcomeMessage) {
+            setWelcomeMessageIndex(prevIndex => prevIndex + 1 >= WELCOME_MESSAGES.length ? 0 : prevIndex + 1);
+            hasResetWelcomeMessage = true;
+          }
+          opacity = (elapsed - FADE_IN_POINT) / (ANIMATION_DURATION - FADE_IN_POINT);
         }
+        welcomeHeader.current.style.opacity = opacity;
       }
 
+      if(elapsed > ANIMATION_DURATION) {
+        start = undefined;
+        hasResetWelcomeMessage = false;
+      }
       animationId = requestAnimationFrame(animationCallback);
-
-      timeoutId = setTimeout(() => {
-        setWelcomeMessageIndex(prevIndex => prevIndex + 1 >= WELCOME_MESSAGES.length ? 0 : prevIndex + 1);
-        setIsFadeOut(false);
-      }, 1000);
     }
 
-    return () => clearTimeout(timeoutId);
-  }, [ isFadeOut ]);
+    animationId = requestAnimationFrame(animationCallback);
+
+    return () => cancelAnimationFrame(animationId);
+  }, []);
 
   return (
     <div className="welcomeBanner">
