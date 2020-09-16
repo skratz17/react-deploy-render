a14e9d5c253a07d9fff2bfd3746e23b8d51b5bb0
Jacob Eckert <jweckert17@gmail.com>
2020-09-16 16:55:24 -0500

    initial concept for login register grow shrink done

--- src/components/auth/LoginAndRegister/LoginAndRegister.js
+++ src/components/auth/LoginAndRegister/LoginAndRegister.js
@@ -8,12 +8,19 @@ const LoginAndRegister = props => {
   const [ isLoginMode, setIsLoginMode ] = useState(true);
 
   return (
-    <div className="loginAndRegister">
-      { isLoginMode && <LoginForm {...props} /> }
+    <div className={`loginAndRegister ${isLoginMode ? 'loginMode' : 'registerMode'}`}>
+      <div className={`loginFormWrapper ${isLoginMode ? '' : 'hidden'}`}>
+        <LoginForm {...props} />
+      </div>
+      {/* { isLoginMode && <LoginForm {...props} /> } */}
       <button className={`loginAndRegister__loginModeToggleButton btn ${isLoginMode ? 'btn--create' : 'btn--back'}`} onClick={() => setIsLoginMode(!isLoginMode)}>
         { isLoginMode ? 'Create Account' : 'Back to Login' }
       </button>
-      { !isLoginMode && <RegisterForm {...props} /> }
+      
+      <div className={`registerFormWrapper ${isLoginMode ? 'hidden' : ''}`}>
+        <RegisterForm {...props} />
+      </div>
+      {/* { !isLoginMode && <RegisterForm {...props} /> } */}
     </div>
   );
 };
