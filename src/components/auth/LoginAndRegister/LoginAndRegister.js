ee574c88b2c69b10438a0a9c095d9c89ef42e43b
Jacob Eckert <jweckert17@gmail.com>
2020-09-16 17:21:48 -0500

    sliding is smoother, unnecessary styles removed

--- src/components/auth/LoginAndRegister/LoginAndRegister.js
+++ src/components/auth/LoginAndRegister/LoginAndRegister.js
@@ -9,14 +9,15 @@ const LoginAndRegister = props => {
 
   return (
     <div className={`loginAndRegister ${isLoginMode ? 'loginMode' : 'registerMode'}`}>
-      <div className={`loginFormWrapper ${isLoginMode ? '' : 'hidden'}`}>
+      <div className="loginFormWrapper">
         <LoginForm {...props} />
       </div>
+
       <button className={`loginAndRegister__loginModeToggleButton btn ${isLoginMode ? 'btn--create' : 'btn--back'}`} onClick={() => setIsLoginMode(!isLoginMode)}>
         { isLoginMode ? 'Create Account' : 'Back to Login' }
       </button>
       
-      <div className={`registerFormWrapper ${isLoginMode ? 'hidden' : ''}`}>
+      <div className="registerFormWrapper">
         <RegisterForm {...props} />
       </div>
     </div>
