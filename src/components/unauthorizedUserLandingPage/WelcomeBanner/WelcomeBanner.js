434f6dcdaf8612f75d9971b467e5551b7c9c1e31
Jacob Eckert <jweckert17@gmail.com>
2020-09-16 13:09:19 -0500

    setup cycling welcome message in WelcomeBanner

--- src/components/unauthorizedUserLandingPage/WelcomeBanner/WelcomeBanner.js
+++ src/components/unauthorizedUserLandingPage/WelcomeBanner/WelcomeBanner.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+const WELCOME_MESSAGES = [
+  'Welcome to UHHHWUT',
+  'Bienvenue à UHHHWUT',
+  'UHHHWUTへようこそ',
+  '歡迎來到UHHHWUT'
+];
 
 const WelcomeBanner = () => {
+  const [ welcomeMessageIndex, setWelcomeMessageIndex ] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setWelcomeMessageIndex(prevIndex => prevIndex + 1 >= WELCOME_MESSAGES.length ? 0 : prevIndex + 1);
+    }, 3000);
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <div className="welcomeBanner">
-      <h2 className="welcomeBanner__header">Welcome to UHHHWUT</h2>
+      <h2 className="welcomeBanner__header">{WELCOME_MESSAGES[welcomeMessageIndex]}</h2>
       <p className="welcomeBanner__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis doloribus unde reprehenderit voluptates eveniet aliquam sapiente, quos quisquam expedita ipsum nemo repudiandae autem sed quidem, nam iste? Maxime, alias commodi!</p>
     </div>
   );
