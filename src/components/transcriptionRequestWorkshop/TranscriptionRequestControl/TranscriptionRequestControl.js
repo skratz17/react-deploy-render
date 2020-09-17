e284ca7bf4a7d00005a2df36e786d743e5c438cb
Jacob Eckert <jweckert17@gmail.com>
2020-09-17 10:45:28 -0500

    adding icons for transcription request control button

--- src/components/transcriptionRequestWorkshop/TranscriptionRequestControl/TranscriptionRequestControl.js
+++ src/components/transcriptionRequestWorkshop/TranscriptionRequestControl/TranscriptionRequestControl.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import recordImage from '../../../assets/record.svg';
+import stopImage from '../../../assets/stop.svg';
+
 const TranscriptionRequestControl = props => {
   const { isRequesting, onClick, onCancel } = props;
 
   return (
     <div className="transcriptionRequestControlWrapper">
+      <p>{isRequesting ? 'Stop Transcription Request' : 'Create Transcription Request'}</p>
+      <button className="transcriptionRequestControl" onClick={onClick}>
+        <img src={isRequesting ? stopImage : recordImage} alt={isRequesting ? 'Stop Request' : 'Create Request'}/>
+      </button>
       { isRequesting && 
         <button className="transcriptionRequestCancel" onClick={onCancel}>
           Cancel
         </button>
       }
-      <button className="transcriptionRequestControl" onClick={onClick}>
-        { isRequesting ? 'Stop Transcription Request' : 'Create Transcription Request' }
-      </button>
     </div>
   );
 };
