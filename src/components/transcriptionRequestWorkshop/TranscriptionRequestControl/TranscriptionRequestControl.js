6bc157aa1af1421c91dface1a797ef4d9c03c7e8
Jacob Eckert <jweckert17@gmail.com>
2020-09-17 11:04:24 -0500

    transcription request control styling looking pretty solid

--- src/components/transcriptionRequestWorkshop/TranscriptionRequestControl/TranscriptionRequestControl.js
+++ src/components/transcriptionRequestWorkshop/TranscriptionRequestControl/TranscriptionRequestControl.js
@@ -3,21 +3,27 @@ import PropTypes from 'prop-types';
 
 import recordImage from '../../../assets/record.svg';
 import stopImage from '../../../assets/stop.svg';
+import './TranscriptionRequestControl.css';
 
 const TranscriptionRequestControl = props => {
   const { isRequesting, onClick, onCancel } = props;
 
   return (
     <div className="transcriptionRequestControlWrapper">
-      <p>{isRequesting ? 'Stop Transcription Request' : 'Create Transcription Request'}</p>
-      <button className="transcriptionRequestControl" onClick={onClick}>
-        <img src={isRequesting ? stopImage : recordImage} alt={isRequesting ? 'Stop Request' : 'Create Request'}/>
-      </button>
-      { isRequesting && 
-        <button className="transcriptionRequestCancel" onClick={onCancel}>
-          Cancel
+      <p className="transcriptionRequestControl__prompt">{isRequesting ? 'Stop Transcription Request' : 'Start Transcription Request'}</p>
+      <div className="transcriptionRequestControl__buttonsWrapper">
+        <button className="transcriptionRequestControl" onClick={onClick}>
+          <img className="transcriptionRequestControl__icon" src={isRequesting ? stopImage : recordImage} alt={isRequesting ? 'Stop Request' : 'Start Request'}/>
         </button>
-      }
+        { isRequesting && 
+          <>
+            <p className="transcriptionRequestCancel__prompt">OR</p>
+            <button className="btn btn--back transcriptionRequestCancel" onClick={onCancel}>
+              Cancel
+            </button>
+          </>
+        }
+      </div>
     </div>
   );
 };
