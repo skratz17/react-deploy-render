e118b610ef1120c89b894a1d278668a35a3592c2
Jacob Eckert <jweckert17@gmail.com>
2020-09-18 13:17:45 -0500

    wrapping app in IntlProvider, wrapping static text in NavBar in FormattedMessage as proof of concept

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
+import { FormattedMessage } from 'react-intl';
 
 import './NavBar.css';
 
@@ -7,9 +8,9 @@ const NavBar = props => {
   const location = useLocation();
 
   const linksConfig = [
-    { text: 'Workshop', to: '/workshop' },
-    { text: 'Dashboard', to: '/dashboard' },
-    { text: 'Logout', to: '/logout' }
+    { text: 'Workshop', id: 'workshopLink', to: '/workshop' },
+    { text: 'Dashboard', id: 'dashboardLink', to: '/dashboard' },
+    { text: 'Logout', id: 'logoutLink', to: '/logout' }
   ];
 
   return (
@@ -17,7 +18,10 @@ const NavBar = props => {
       <ul className="navBar__links">
         { linksConfig.map(l => (
           <li key={l.to} className={`navBar__linksListItem ${l.to === location.pathname ? 'current' : ''}`}>
-            <Link className="navBar__link" to={l.to}>{l.text}</Link>
+            <Link className="navBar__link" to={l.to}>
+              <FormattedMessage id={`navBar.${l.id}`}
+                defaultMessage={l.text} />
+            </Link>
           </li>
         ))}
       </ul>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { IntlProvider } from 'react-intl';
 
 import UhhhWut from './components/UhhhWut';
 import './index.css';
@@ -8,7 +9,9 @@ import './index.css';
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <UhhhWut />
+      <IntlProvider>
+        <UhhhWut />
+      </IntlProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
