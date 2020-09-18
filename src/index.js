d05ce561aea9a7ebd49ea7a97b8bd9925fbd56e8
Jacob Eckert <jweckert17@gmail.com>
2020-09-18 14:27:31 -0500

    full proof of concept of internationalization complete

--- src/index.js
+++ src/index.js
@@ -4,12 +4,28 @@ import { BrowserRouter } from 'react-router-dom';
 import { IntlProvider } from 'react-intl';
 
 import UhhhWut from './components/UhhhWut';
+import localeData from './locales.json';
 import './index.css';
+
+const DEFAULT_LANGUAGE = 'en';
+
+// figure out the user's language acc. to browser settings
+const language = (navigator.languages && navigator.languages[0]) ||
+  navigator.language ||
+  navigator.userLanguage;
+
+// remove the region code from their language (e.g., en-US -> en)
+const languageWithoutRegionCode = language.toLowerCase().split(/[_-]+/)[0];
+
+// get the set of messages for that language, or fall back to DEFAULT_LANGUAGE messages if not found
+const messages = localeData[languageWithoutRegionCode] ||
+  localeData[language] ||
+  localeData[DEFAULT_LANGUAGE];
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <IntlProvider>
+      <IntlProvider locale={language} messages={messages}>
         <UhhhWut />
       </IntlProvider>
     </BrowserRouter>
