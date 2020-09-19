19d45c8bbd543bc7f1441f447f4ac6968f694731
Jacob Eckert <jweckert17@gmail.com>
2020-09-19 11:36:02 -0500

    all static content has been marked for internationalization

--- src/components/transcriptionRequestWorkshop/TranscriptionRequestControl/TranscriptionRequestControl.js
+++ src/components/transcriptionRequestWorkshop/TranscriptionRequestControl/TranscriptionRequestControl.js
@@ -1,25 +1,51 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FormattedMessage, defineMessages, injectIntl } from 'react-intl';
 
 import recordImage from '../../../assets/record.svg';
 import stopImage from '../../../assets/stop.svg';
 import './TranscriptionRequestControl.css';
+
+defineMessages({
+  startRequestAltText: {
+    id: 'transcriptionRequestControl.startRequestAltText',
+    defaultMessage: 'Start Request'
+  },
+  stopRequestAltText: {
+    id: 'transcriptionRequestControl.stopRequestAltText',
+    defaultMessage: 'Stop Request'
+  }
+});
 
 const TranscriptionRequestControl = props => {
   const { isRequesting, disabled, onClick, onCancel } = props;
 
   return (
     <div className="transcriptionRequestControlWrapper">
-      <p className="transcriptionRequestControl__prompt">{isRequesting ? 'Stop Transcription Request' : 'Start Transcription Request'}</p>
+      <p className="transcriptionRequestControl__prompt">
+        {isRequesting ? 
+          <FormattedMessage id="transcriptionRequestControl.stopTranscriptionRequestPrompt"
+            defaultMessage="Stop Transcription Request" /> : 
+          <FormattedMessage id="transcriptionRequestControl.startTranscriptionRequestPrompt"
+            defaultMessage="Start Transcription Request" />
+        }
+      </p>
       <div className="transcriptionRequestControl__buttonsWrapper">
         <button className="transcriptionRequestControl" disabled={disabled} onClick={onClick}>
-          <img className="transcriptionRequestControl__icon" src={isRequesting ? stopImage : recordImage} alt={isRequesting ? 'Stop Request' : 'Start Request'}/>
+          <img className="transcriptionRequestControl__icon" 
+            src={isRequesting ? stopImage : recordImage} 
+            alt={isRequesting ? 
+              props.intl.formatMessage({ id: 'transcriptionRequestControl.stopRequestAltText' }) : 
+              props.intl.formatMessage({ id: 'transcriptionRequestControl.startRequestAltText' })
+            }
+          />
         </button>
         { isRequesting && 
           <>
             <p className="transcriptionRequestCancel__prompt">OR</p>
             <button className="btn btn--back transcriptionRequestCancel" onClick={onCancel}>
-              Cancel
+              <FormattedMessage id="transcriptionRequestControl.cancelTranscriptionRequestButton"
+                defaultMessage="Cancel" />
             </button>
           </>
         }
@@ -35,4 +61,4 @@ TranscriptionRequestControl.propTypes = {
   onCancel: PropTypes.func
 };
 
-export default TranscriptionRequestControl;
+export default injectIntl(TranscriptionRequestControl);
