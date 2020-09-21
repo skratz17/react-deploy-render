ee16c48721dcda1a89997555230222c734e498df
Jacob Eckert <jweckert17@gmail.com>
2020-09-21 11:36:44 -0500

    getting the current player time in yet another way, displaying transcription for currently playing video segment in the workshop if exists

---
+++ src/components/transcriptionRequestWorkshop/TranscriptionForSegment/TranscriptionForSegment.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FormattedMessage } from 'react-intl';
+
+import './TranscriptionForSegment.css';
+
+const TranscriptionForSegment = props => {
+  const { transcription } = props;
+
+  return (
+    <div className={`transcriptionForSegment ${transcription && transcription.transcription ? '' : 'hidden'}`}>
+      <h3 className="transcriptionForSegment__header">
+        <FormattedMessage id="transcriptionForSegment.header"
+          defaultMessage="Transcription of Current Segment" />
+      </h3>
+      { transcription && 
+        <p className="transcriptionForSegment__text">{transcription.transcription}</p>
+      }
+    </div>
+  );
+};
+
+TranscriptionForSegment.propTypes = {
+  transcription: PropTypes.object
+};
+
+export default TranscriptionForSegment;
--- src/components/transcriptionRequestWorkshop/TranscriptionRequestWorkshop.js
+++ src/components/transcriptionRequestWorkshop/TranscriptionRequestWorkshop.js
@@ -4,6 +4,7 @@ import { FormattedMessage } from 'react-intl';
 
 import { TranscriptionRequestContext } from '../transcriptionRequest/TranscriptionRequestProvider';
 import YouTubeSearchBar from '../youTubeSearchBar/YouTubeSearchBar';
+import TranscriptionForSegment from './TranscriptionForSegment/TranscriptionForSegment';
 import TranscriptionRequestControl from './TranscriptionRequestControl/TranscriptionRequestControl';
 import TranscriptionRequestRecordingPreview from './TranscriptionRequestRecordingPreview/TranscriptionRequestRecordingPreview';
 import TranscriptionRequestList from '../transcriptionRequest/TranscriptionRequestList/TranscriptionRequestList';
@@ -18,6 +19,7 @@ const TranscriptionRequestWorkshop = () => {
   const [ currentPlayerTime, setCurrentPlayerTime ] = useState(null);
   const [ hasEndTimeError, setHasEndTimeError ] = useState(false);
   const [ transcriptionRequestsForVideo, setTranscriptionRequestsForVideo ] = useState([]);
+  const [ transcriptionForSegment, setTranscriptionForSegment ] = useState(null);
   const [ activatingTranscriptionRequestId, setActivatingTranscriptionRequestId ] = useState(null);
 
   const { transcriptionRequests, getTranscriptionRequests, saveTranscriptionRequest } = useContext(TranscriptionRequestContext);
@@ -45,22 +47,38 @@ const TranscriptionRequestWorkshop = () => {
   }, [ transcriptionRequests, videoId ]);
 
   useEffect(() => {
-    if(startTime && !currentPlayerTime) {
-      setCurrentPlayerTime(Math.ceil(player.getCurrentTime()));
+    if(isVideoPlaying) {
       const intervalId = setInterval(() => {
         setCurrentPlayerTime(Math.ceil(player.getCurrentTime()));
       }, 1000);
 
       return () => clearInterval(intervalId);
     }
-    else {
-      setCurrentPlayerTime(null);
+  }, [ isVideoPlaying ]);
+
+  useEffect(() => {
+    if(currentPlayerTime !== null) {
+      const transcriptionRequestForSegment = transcriptionRequestsForVideo.find(
+        tR => currentPlayerTime >= tR.startTime && currentPlayerTime <= tR.endTime
+      );
+
+      if(transcriptionRequestForSegment && transcriptionRequestForSegment.transcriptions && transcriptionRequestForSegment.transcriptions.length) {
+        setTranscriptionForSegment(transcriptionRequestForSegment.transcriptions[0]);
+      }
+      else {
+        setTranscriptionForSegment(null);
+      }
     }
-  }, [ startTime ]);
+  }, [ currentPlayerTime ]);
 
   const handleYouTubeSearchBarChange = videoId => {
     setVideoId(videoId);
     setStartTime(null);
+  };
+
+  const handleYouTubeStateChange = e => {
+    setIsVideoPlaying(e.data === YouTube.PlayerState.PLAYING);
+    setCurrentPlayerTime(Math.ceil(player.getCurrentTime()));
   };
 
   const handleTranscriptionRequestControlClick = async () => {
@@ -105,10 +123,12 @@ const TranscriptionRequestWorkshop = () => {
         <div style={{ height: youtubePlayerOpts.height + 'px', width: youtubePlayerOpts.width + 'px' }}>
           <YouTube videoId={videoId} 
             onReady={e => setPlayer(e.target)} 
-            onStateChange={e => setIsVideoPlaying(e.data === YouTube.PlayerState.PLAYING)}
+            onStateChange={handleYouTubeStateChange}
             opts={youtubePlayerOpts}
             />
         </div>
+
+        <TranscriptionForSegment transcription={transcriptionForSegment} />
       </div>
 
       <div className="col--right">
