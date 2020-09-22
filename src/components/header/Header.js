d844c0ac2da0a4110bc6a2715c8a6fcd66a18960
Jacob Eckert <jweckert17@gmail.com>
2020-09-22 16:27:38 -0500

    setting up initial color palette, moving navbar into header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { defineMessages, injectIntl } from 'react-intl';
+import { Link } from 'react-router-dom';
 
+import NavBar from '../navbar/NavBar';
 import logo from '../../assets/logo.png';
 import './Header.css';
 
@@ -14,7 +16,10 @@ defineMessages({
 const Header = props => (
   <header className="header">
     <img className="logo" src={logo} alt={props.intl.formatMessage({ id: 'header.logoAltText' })} />
-    <h1 className="siteName">uhhh wut</h1>
+    <h1 className="siteName">
+      <Link className="siteName__link" to="/">uhhh wut</Link>
+    </h1>
+    <NavBar />
   </header>
 );
 
