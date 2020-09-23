c449aaac2b247463d164e996ea3a111cfcbd4632
Jacob Eckert <jweckert17@gmail.com>
2020-09-23 09:19:30 -0500

    simplifying that function makes me not really have to forbid input in my time input anymore

--- src/components/form/timeInput/TimeInput.js
+++ src/components/form/timeInput/TimeInput.js
@@ -2,17 +2,8 @@ import React from 'react';
 
 import { formatToMSSTimeString, convertTimeStringToSeconds, convertSecondsToTimeString } from '../../../utils/timeFormatters';
 
-const VALID_KEYS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'Delete', 'Backspace', 'Tab', 'Down', 'ArrowDown', 'Up', 'ArrowUp', 'Left', 'ArrowLeft', 'Right', 'ArrowRight', 'Esc', 'Escape', 'Fn', 'Control', 'NumLock', 'Shift'];
-
 const TimeInput = props => {
   const { onChange, value } = props;
-
-  const handleKeyDown = e => {
-    if(!VALID_KEYS.includes(e.key)) {
-      e.stopPropagation();
-      e.preventDefault();
-    }
-  };
 
   const handleChange = e => {
     const timeString = formatToMSSTimeString(e.target.value);
@@ -24,7 +15,6 @@ const TimeInput = props => {
     <input {...props}
       type="text"
       value={convertSecondsToTimeString(value)}
-      onKeyDown={handleKeyDown}
       onChange={handleChange} />
   );
 };
